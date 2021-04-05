eddb9af99ed999bdf0db76b319edc79548f411b9
nmjr-311 <nmjr31@gmail.com>
2021-04-05 11:55:11 +0900

    fix: use correct module name in Test

--- test/MyLibSpec.hs
+++ test/MyLibSpec.hs
@@ -1,4 +1,8 @@
-module Main (main) where
+module MyLibSpec where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+import           Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = describe "trivial" $ do
+  it "True should be True" $ do
+    True `shouldBe` True
